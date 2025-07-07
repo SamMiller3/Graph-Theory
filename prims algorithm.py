88b16cb24614b5383e385e14c87f121ab407ca88
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-07 15:54:32 +0100

    Update prims algorithm.py

--- prims algorithm.py
+++ prims algorithm.py
@@ -1,8 +1,8 @@
-# Prims algorithm to find a minimum spanning tree given a distance matrix
+# Prims algorithm to find a minimum spanning tree given an adjacency matrix
 # 27/06/2025
 
 # a value of 999999 denotes null or infinity
-distance_matrix=[
+adjacency_matrix=[
     [999999,16,12,21,999999,999999,999999],
     [16,999999,999999,17,20,999999,999999],
     [12,999999,999999,28,999999,31,999999],
@@ -15,24 +15,24 @@ distance_matrix=[
 # look along the rows, and delete columns (by replacing with 999999)
 current_row=0
 MST_weight=0
-for row in distance_matrix:
+for row in adjacency_matrix:
         row[0] = 999999
 MST="A" # start at A
 visited_nodes=[0]
 
-for i in range(len(distance_matrix)-1):
+for i in range(len(adjacency_matrix)-1):
     # find next node
     distance=999999
     for node in visited_nodes:
-        if distance>min(distance_matrix[node]):
-            distance=min(distance_matrix[node])
-            current_column=distance_matrix[node].index(distance)
+        if distance>min(adjacency_matrix[node]):
+            distance=min(adjacency_matrix[node])
+            current_column=adjacency_matrix[node].index(distance)
 
     MST+=chr(65+current_column)
     MST_weight+=distance
     visited_nodes.append(current_column)
     # eliminate current column
-    for row in distance_matrix:
+    for row in adjacency_matrix:
         row[current_column] = 999999
         
 print(MST, MST_weight)
