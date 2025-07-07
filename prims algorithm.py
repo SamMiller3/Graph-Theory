44a6052121a3c386489a9b7731a894549415b68b
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-08 00:41:40 +0100

    Update prims algorithm.py

--- prims algorithm.py
+++ prims algorithm.py
@@ -1,28 +1,29 @@
 # Prims algorithm to find a minimum spanning tree given an adjacency matrix
 # 27/06/2025
 
-# a value of 999999 denotes null or infinity
+INF = float('inf')
+
 adjacency_matrix=[
-    [999999,16,12,21,999999,999999,999999],
-    [16,999999,999999,17,20,999999,999999],
-    [12,999999,999999,28,999999,31,999999],
-    [21,17,28,999999,18,19,23],
-    [999999,20,999999,18,999999,999999,11],
-    [999999,999999,31,19,999999,999999,27],
-    [999999,999999,999999,23,11,27,999999]
+    [INF,16,12,21,INF,INF,INF],
+    [16,INF,INF,17,20,INF,INF],
+    [12,INF,INF,28,INF,31,INF],
+    [21,17,28,INF,18,19,23],
+    [INF,20,INF,18,INF,INF,11],
+    [INF,INF,31,19,INF,INF,27],
+    [INF,INF,INF,23,11,27,INF]
 ]
 
-# look along the rows, and delete columns (by replacing with 999999)
+# look along the rows, and delete columns (by replacing with INF)
 current_row=0
 MST_weight=0
 for row in adjacency_matrix:
-        row[0] = 999999
+        row[0] = INF
 MST="A" # start at A, assumes each node is laballed A, B, C in order in the matrix
 visited_nodes=[0]
 
 for i in range(len(adjacency_matrix)-1):
     # find next node
-    distance=999999
+    distance=INF
     for node in visited_nodes:
         if distance>min(adjacency_matrix[node]):
             distance=min(adjacency_matrix[node])
@@ -33,7 +34,7 @@ for i in range(len(adjacency_matrix)-1):
     visited_nodes.append(current_column)
     # eliminate current column
     for row in adjacency_matrix:
-        row[current_column] = 999999
+        row[current_column] = INF
         
 print(MST, MST_weight)
     
