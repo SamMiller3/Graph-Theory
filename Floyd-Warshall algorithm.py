92b18f9489637bdac483d3e08b93987f6746c4b1
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-08 00:40:37 +0100

    Update Floyd-Warshall algorithm.py

--- Floyd-Warshall algorithm.py
+++ Floyd-Warshall algorithm.py
@@ -1,30 +1,34 @@
-#Floyd-Warshall algorithm to find a least distance matrix given an adjacency matrix
-# 07/07/25
+#Floyd-Warshall algorithm 07/07/25
 
 import numpy as np
 
-INF = 999999 # a value of 999999 denotes null or infinity (there is no direct connection)
+INF = float('inf') # a value of INF denotes not adjacent
 # 0 is used for distance to reach itself
 adjacency_matrix= np.array([
-    [0,15,7,18,3],
-    [15,0,INF,INF,INF],
-    [7,5,0,4,9],
-    [18,INF,4,0,3],
-    [INF,INF,9,3,0]
+    [0,4,7,INF],
+    [4,0,INF,9],
+    [7,INF,0,INF],
+    [1,9,INF,0],
 ]) 
 
 n = len(adjacency_matrix)
-predecessor_matrix = np.tile(np.arange(1, n+1), (n, 1)) # initialise 
+route_matrix = np.tile(np.arange(1, n+1), (n, 1)) # initialise 
+# A table where each cell route[i][j] tells you the next hop to reach node j starting from node i.
 distance_matrix = adjacency_matrix.copy()
 
 for k in range(n): # intermediate values
     for i in range(n): # rows
         for j in range(n): # columns
             if distance_matrix[i,k]+distance_matrix[k,j] < distance_matrix[i,j]: # update shortest route based on intermediate
-                distance_matrix[i,j] = distance_matrix[i,k]+distance_matrix[k,j]
-                predecessor_matrix[i,j] = k+1
+                distance_matrix[i,j] = int(distance_matrix[i,k]+distance_matrix[k,j])
+                route_matrix[i,j] = k+1
+    
+    print("least distance matrix:")
+    print(distance_matrix)
+    print("route matrix:")
+    print(route_matrix)
 
 print("least distance matrix:")
 print(distance_matrix)
-print("predecessor matrix:")
-print(predecessor_matrix)
+print("route matrix:")
+print(route_matrix)
