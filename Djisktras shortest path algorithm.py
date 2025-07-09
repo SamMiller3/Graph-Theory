a9babede935c9fea1435861342c314d15bbe72da
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-09 20:43:20 +0100

    fixed bug returning distance

--- Djisktras shortest path algorithm.py
+++ Djisktras shortest path algorithm.py
@@ -15,18 +15,21 @@ adj_mat= np.array([
     [INF,INF,113,INF,INF,41,10,0]
 ])  # edit this adj matrix to construct a different graph
 
-start_node = int(input("what is the start node? (eg 0, 1, 5, 7 note it is 0 indexed)"))
+start_node = int(input("what is the start node? (eg 0, 1, 5, 7 note it is 0 indexed): "))
 tent_mat = adj_mat.copy()
 predecessor = {} # store the predecessor to construct the path at the end
+distances = {} # store the distance to each node from the source
 
-visited_nodes = [[start_node,0]] # 2D array, first column is node, second column is distance from source
+visited_nodes = [start_node]
+distances[start_node]=[0]
 tent_mat[:, start_node] = INF # delete first column so not to revist
 predecessor[start_node] = start_node 
 
 # find initial tentative distance
 
 next_node, min_distance = np.argmin(tent_mat[start_node]) , np.min(tent_mat[start_node])
-visited_nodes.append([next_node , min_distance])
+visited_nodes.append(next_node)
+distances[next_node] = min_distance
 tent_mat[:, next_node] = INF # delete next column
 tent_mat[next_node] += min_distance # update distance
 predecessor[next_node] = start_node
@@ -34,11 +37,12 @@ predecessor[next_node] = start_node
 while len(visited_nodes)!=len(adj_mat):
     min_distance = INF
     for node in visited_nodes:
-        if min_distance > np.min(tent_mat[node[0]]): # check each visited node to see closest adj node
-            min_distance = np.min(tent_mat[node[0]]) # store distance
-            next_node = np.argmin(tent_mat[node[0]]) # and next node
-            prev_node = node[0] # store predecessor
-    visited_nodes.append([next_node,min_distance])
+        if min_distance > np.min(tent_mat[node]): # check each visited node to see closest adj node
+            min_distance = np.min(tent_mat[node]) # store distance
+            next_node = np.argmin(tent_mat[node]) # and next node
+            prev_node = node # store predecessor
+    visited_nodes.append(next_node)
+    distances[next_node] = min_distance
     tent_mat[:, next_node] = INF # delete next column
     tent_mat[next_node] += min_distance # update distance
     predecessor[next_node] = prev_node
@@ -46,11 +50,11 @@ while len(visited_nodes)!=len(adj_mat):
 target_node = int(input("what is the target node? "))
 
 # construct path
-prev_node = next_node
+prev_node = target_node
 path = str(prev_node)
 while predecessor[prev_node] != start_node:
     prev_node = predecessor[prev_node]
     path = str(prev_node) + path
 path = str(start_node) + path
 print(f"shortest path is: {path}")
-print(f"distance is {visited_nodes[target_node][1]}")
+print(f"distance is {distances[target_node]}")
