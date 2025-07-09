0d50e1768404345747c12cd207b2d66c59e0e9da
Samuel <58521841+SamMiller3@users.noreply.github.com>
2025-07-09 21:38:54 +0100

    Update Djisktras shortest path algorithm.py

--- Djisktras shortest path algorithm.py
+++ Djisktras shortest path algorithm.py
@@ -1,5 +1,8 @@
 # Djisktras shortest path algorithm implemented using dynamic programming on an adjacency matrix
 # 08/07/2025 - 09/07/2025
+
+# Djisktras shortest path algorithm
+
 import numpy as np
 
 INF = float('inf') # a value of INF denotes not adjacent
@@ -16,27 +19,28 @@ adj_mat= np.array([
 ])  # edit this adj matrix to construct a different graph
 
 start_node = int(input("what is the start node? (eg 0, 1, 5, 7 note it is 0 indexed): "))
-tent_mat = adj_mat.copy()
+dis_mat = adj_mat.copy() # Working matrix that gets updated with cumulative distances from source
 predecessor = {} # store the predecessor to construct the path at the end
 distances = {} # store the distance to each node from the source
 
 visited_nodes = [start_node]
 distances[start_node]=[0]
-tent_mat[:, start_node] = INF # delete first column so not to revist
+dis_mat[:, start_node] = INF # delete first column so not to revist
 predecessor[start_node] = start_node 
 
-
+# Main algorithm loop - visit all nodes
 while len(visited_nodes)!=len(adj_mat):
     min_distance = INF
+    # Find closest unvisited node from any visited node
     for node in visited_nodes:
-        if min_distance > np.min(tent_mat[node]): # check each visited node to see closest adj node
-            min_distance = np.min(tent_mat[node]) # store distance
-            next_node = np.argmin(tent_mat[node]) # and next node
+        if min_distance > np.min(dis_mat[node]): # check each visited node to see closest adj node
+            min_distance = np.min(dis_mat[node]) # store distance
+            next_node = np.argmin(dis_mat[node]) # and next node
             prev_node = node # store predecessor
     visited_nodes.append(next_node)
     distances[next_node] = min_distance
-    tent_mat[:, next_node] = INF # delete next column so not to visit it twice
-    tent_mat[next_node] += min_distance # update tentative distances
+    dis_mat[:, next_node] = INF # delete next column so not to visit it twice
+    dis_mat[next_node] += min_distance # Update all edges from this node with cumulative distance from source
     predecessor[next_node] = prev_node
 
 target_node = int(input("what is the target node? "))
@@ -47,6 +51,6 @@ path = str(prev_node)
 while predecessor[prev_node] != start_node:
     prev_node = predecessor[prev_node]
     path = str(prev_node) + "-" + path
-path = str(start_node) + "-" path
+path = str(start_node) + "-" + path
 print(f"shortest path is: {path}")
 print(f"distance is {distances[target_node]}")
